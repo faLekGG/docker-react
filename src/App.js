949f583b7fc1606d0e9ed3fc22f7a2402ac666a8
faLekGG <falek1994@mail.ru>
2019-03-08 22:41:53 +0300

    start working with feature branch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I have already edited <code>src/App.js</code> 
+            The feature branch<code>src/App.js</code> 
           </p>
           <a
             className="App-link"
